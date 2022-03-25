7039335ac737a6307f29b199338397c8ce5f956e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2022-03-25 15:02:27 -0400

    adding webhook controller

---
+++ src/WebHookApi/Controllers/MyWebHook.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebHookApi.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyWebHook : ControllerBase {
+        [HttpPost]
+        public async Task PostHookAsync([FromBody] object body)
+        {
+            Console.WriteLine($"PostHook called");
+
+            //throw new NullReferenceException();
+            //Console.WriteLine($"going to sleep forever");
+            //while (true) {
+            //    Console.Write(".");
+            //    await Task.Delay(5000);
+            //}
+        }
+    }
+}
